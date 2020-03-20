54032e76b675f5fa0c6bffe5e4332d40fd492d0f
Kushal Pandya <kupandya@in.ibm.com>
2020-03-20 17:15:35 +0530

    added No news found test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Home from './pages/Home';
 
 test('renders without crashing', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
+});
+
+test('when there are no news, no news message should show', async () => {
+  const { findByText } = render(<Home />);
+  await findByText('No news found')
 });
